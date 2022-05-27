9e08ebc27fa0cea744cc9c68e2df56223b755f01
noreplay <noreplay>
2022-05-27 14:43:58 +0530

    line chart added

--- cars.py
+++ cars.py
@@ -26,13 +26,33 @@ def process_data():
     sortesd_dic= sorted(dic.items(), key=lambda item: item[1])
     tmp=str(sortesd_dic[-1]).strip('()')
     pop_year,pop_sales= tmp.split(',')
-    
+
     summary=[
         f"Total revenue made = {total_revenue}",
         f"{total_saled_brand} {total_saled_model} is most saled car and sales was {total_sales}",
         f"Popuar year was {pop_year} and sales of that year {pop_sales}",
     ]
-    return summary
+
+    #making sales list by year
+    year_list=[]
+    data_list=[]
+    data_tup=tuple
+    i=0
+    i1=True
+    for k in sorted(dic) :
+        if i==0 and i1==True:
+            year_list.append(k)
+            data_list.append(dic[k])       
+            i1=False
+        if i==2 :
+            year_list.append(k)
+            data_list.append(dic[k])
+            i=0
+        i+=1
+    data_tup=tuple(data_list)
+    #print(year_list,data_tup)
+
+    return summary,year_list,data_tup
 
 def dic_to_lst():
     lst=[]
@@ -56,6 +76,12 @@ def create_report(summary):
     report.generate_report(file,title,contents, table_content)
     print('report ok')
 
+def create_linechart(year,data):
+    file='docs/line_chart.pdf'
+    summary='Sales chart'
+    report.generate_linechart(file=file,summary=summary,year_list=year,data_list=data)
+    print('line chart ok')
+
 def send_email(summary):
     sender=str(input('enter sender mail id : '))
     recipient= str(input('enter recipient mail id : '))
@@ -72,6 +98,7 @@ if __name__ == '__main__' :
     with open('tmp/car_details.json') as j:
         cars = json.load(j)
 
-    summary=process_data()
-    create_report(summary)
-    send_email(summary)
+    summary,year,data=process_data()
+    #create_report(summary)
+    #send_email(summary)
+    create_linechart(year,data)
--- report.py
+++ report.py
@@ -4,6 +4,8 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Table, Spacer,Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from reportlab.graphics.charts.linecharts import HorizontalLineChart
+from reportlab.graphics.shapes import Drawing
 
 def generate_report(file,title, summary, table_content ):
     report= SimpleDocTemplate(file)
@@ -17,5 +19,31 @@ def generate_report(file,title, summary, table_content ):
         ('GRID', (0,1), (-1,-1), 2, colors.black),
     ]
     report_table= Table(data=table_content, style=table_style)
-
     report.build([report_title, report_content, report_table])
+
+#line chart
+def generate_linechart(file,summary,data_list,year_list):
+    
+    draw= Drawing(500*500)
+    report=SimpleDocTemplate(file)
+    styles=getSampleStyleSheet()
+    lc=HorizontalLineChart()
+
+    lc.x= 1
+    lc.y= -150
+    lc.height= 300
+    lc.width= 450
+    lc.data=[data_list]
+    lc.joinedLines = 2
+    catname=str(year_list).strip('[]').split(',')
+    lc.categoryAxis.categoryNames=catname
+    lc.categoryAxis.labels.boxAnchor ='n'
+    lc.valueAxis.valueMin=0
+    lc.valueAxis.valueMax=20000
+    lc.valueAxis.valueStep= 2000
+    lc.lines[0].strokeWidth= 2
+    
+    title= Paragraph(summary,styles['h1'])
+    draw.add(lc)
+    report.build([title,draw])
+
